d9a1a434a7a7a1e25ee9a9f56058e23ee6f1034b
chamodhlv <chamodh300@gmail.com>
2026-07-08 00:30:12 +0530

    add TaskMapper interface and implementation, TaskDto record, and basic TaskController setup

--- taskapp/src/main/java/com/task/taskapp/controller/TaskController.java
+++ taskapp/src/main/java/com/task/taskapp/controller/TaskController.java
@@ -1,8 +1,16 @@
 package com.task.taskapp.controller;
 
+import com.task.taskapp.domain.CreateTaskRequest;
+import com.task.taskapp.domain.dto.CreateTaskRequestDto;
+import com.task.taskapp.domain.dto.TaskDto;
+import com.task.taskapp.domain.entity.Task;
 import com.task.taskapp.mapper.TaskMapper;
 import com.task.taskapp.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +27,14 @@ public class TaskController {
         this.taskMapper = taskMapper;
     }
 
-    public ResponseEntity
+    @PostMapping
+    public ResponseEntity<CreateTaskRequestDto> createTask(
+            @Valid @RequestBody CreateTaskRequestDto createTaskRequestDto
+    ) {
+        CreateTaskRequest createTaskRequest = taskMapper.fromDto(createTaskRequestDto);
+        Task task = taskService.createTask(createTaskRequest);
+        TaskDto taskDto = taskMapper.toDto(task);
+        return new ResponseEntity<>(createTaskRequestDto, HttpStatus.CREATED);
+    }
+
 }
